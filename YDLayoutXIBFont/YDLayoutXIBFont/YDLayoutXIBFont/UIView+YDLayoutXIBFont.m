0384ad13d5dd44755faa51eb8825116ede88ffb2
guoyandong <guoyandongemail@163.com>
2019-05-13 12:39:33 +0800

    在UILabel和UIButton的基础上，新增UITextField、UITextView控件，在XIB中拖动创建后，可以根据屏幕大小自动适配文字大小。

--- YDLayoutXIBFont/YDLayoutXIBFont/YDLayoutXIBFont/UIView+YDLayoutXIBFont.m
+++ YDLayoutXIBFont/YDLayoutXIBFont/YDLayoutXIBFont/UIView+YDLayoutXIBFont.m
@@ -23,8 +23,8 @@
         class_replaceMethod(self, @selector(label_awakeFromNib), method_getImplementation(method2), method_getTypeEncoding(method2));
     }
     
-    Method method3 = class_getInstanceMethod([UILabel class], @selector(button_awakeFromNib));
-    Method method4 = class_getInstanceMethod([UILabel class], @selector(awakeFromNib));
+    Method method3 = class_getInstanceMethod([UIButton class], @selector(button_awakeFromNib));
+    Method method4 = class_getInstanceMethod([UIButton class], @selector(awakeFromNib));
     
     if (!class_addMethod([UIButton class], @selector(awakeFromNib), method_getImplementation(method3), method_getTypeEncoding(method3))) {
         method_exchangeImplementations(method4, method3);
@@ -32,9 +32,30 @@
         class_replaceMethod(self, @selector(button_awakeFromNib), method_getImplementation(method4), method_getTypeEncoding(method4));
     }
     
+    Method method5 = class_getInstanceMethod([UITextField class], @selector(button_awakeFromNib));
+    Method method6 = class_getInstanceMethod([UITextField class], @selector(awakeFromNib));
+    
+    if (!class_addMethod([UITextField class], @selector(awakeFromNib), method_getImplementation(method5), method_getTypeEncoding(method5))) {
+        method_exchangeImplementations(method6, method5);
+    } else {
+        class_replaceMethod(self, @selector(textField_awakeFromNib), method_getImplementation(method6), method_getTypeEncoding(method6));
+    }
+    
+    
+    Method method7 = class_getInstanceMethod([UITextView class], @selector(button_awakeFromNib));
+    Method method8 = class_getInstanceMethod([UITextView class], @selector(awakeFromNib));
+    
+    if (!class_addMethod([UITextView class], @selector(awakeFromNib), method_getImplementation(method7), method_getTypeEncoding(method7))) {
+        method_exchangeImplementations(method8, method7);
+    } else {
+        class_replaceMethod(self, @selector(textView_awakeFromNib), method_getImplementation(method8), method_getTypeEncoding(method8));
+    }
+    
+    
 }
 
 /*
+ UILabel
  方法交换以后 当系统调用 awakeFromNib 方法的时候实际上会调用 label_awakeFromNib 方法
  **/
 - (void)label_awakeFromNib {
@@ -45,11 +66,33 @@
 }
 
 /*
+ UIButton
  方法交换以后 当系统调用 awakeFromNib 方法的时候实际上会调用 button_awakeFromNib 方法
  **/
 - (void)button_awakeFromNib {
     [self button_awakeFromNib];
     UIButton * button = (UIButton *)self;
     button.titleLabel.font = [UIFont fontWithDescriptor:button.titleLabel.font.fontDescriptor size:button.titleLabel.font.pointSize * SizeScale];
+}
+
+
+/*
+ UITextField
+ 方法交换以后 当系统调用 awakeFromNib 方法的时候实际上会调用 textField_awakeFromNib 方法
+ **/
+- (void)textField_awakeFromNib {
+    [self textField_awakeFromNib];
+    UITextField * textField = (UITextField *)self;
+    textField.font = [UIFont fontWithDescriptor:textField.font.fontDescriptor size:textField.font.pointSize * SizeScale];
+}
+
+/*
+ UITextView
+ 方法交换以后 当系统调用 awakeFromNib 方法的时候实际上会调用 textView_awakeFromNib 方法
+ **/
+- (void)textView_awakeFromNib {
+    [self textView_awakeFromNib];
+    UITextView * textView = (UITextView *)self;
+    textView.font = [UIFont fontWithDescriptor:textView.font.fontDescriptor size:textView.font.pointSize * SizeScale];
 }
 @end
