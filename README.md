e36a03671311b67fdc0e2be5240a93ca937d7ec5
guoyandong <guoyandongemail@163.com>
2019-05-13 13:20:36 +0800

    新增UISearchBar控件，在XIB中拖动创建后，可以根据屏幕大小自动适配文字大小。

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # YDLayoutXIBFont
 XIB字体等比自动适配
 
-UILabel、UIButton、UITextField、UITextView等控件，在XIB中拖动创建后，可以根据屏幕大小自动适配文字大小。
+UILabel、UIButton、UITextField、UITextView、UISearchBar等控件，在XIB中拖动创建后，可以根据屏幕大小自动适配文字大小。
 
 使用方法：
 直接把YDLayoutXIBFont文件夹下的UIView+YDLayoutXIBFont.h和UIView+YDLayoutXIBFont.m分类文件拖动到项目中即可。
--- YDLayoutXIBFont/YDLayoutXIBFont/UIView+YDLayoutXIBFont.m
+++ YDLayoutXIBFont/YDLayoutXIBFont/UIView+YDLayoutXIBFont.m
@@ -9,7 +9,9 @@
 #import "UIView+YDLayoutXIBFont.h"
 #import <objc/runtime.h>
 #define kScreenWidth [UIScreen mainScreen].bounds.size.width
-#define SizeScale ((CGFloat)(kScreenWidth / 375))
+//#define SizeScale ((CGFloat)(kScreenWidth / 375))
+#define SizeScale ((CGFloat)(kScreenWidth / 1000))
+
 /*
  XIB中Label和UIButton里文字的字体等比自动适配
  **/
@@ -25,31 +27,37 @@
     
     Method method3 = class_getInstanceMethod([UIButton class], @selector(button_awakeFromNib));
     Method method4 = class_getInstanceMethod([UIButton class], @selector(awakeFromNib));
-    
     if (!class_addMethod([UIButton class], @selector(awakeFromNib), method_getImplementation(method3), method_getTypeEncoding(method3))) {
         method_exchangeImplementations(method4, method3);
     } else {
         class_replaceMethod(self, @selector(button_awakeFromNib), method_getImplementation(method4), method_getTypeEncoding(method4));
     }
     
-    Method method5 = class_getInstanceMethod([UITextField class], @selector(button_awakeFromNib));
+    Method method5 = class_getInstanceMethod([UITextField class], @selector(textField_awakeFromNib));
     Method method6 = class_getInstanceMethod([UITextField class], @selector(awakeFromNib));
-    
     if (!class_addMethod([UITextField class], @selector(awakeFromNib), method_getImplementation(method5), method_getTypeEncoding(method5))) {
         method_exchangeImplementations(method6, method5);
     } else {
         class_replaceMethod(self, @selector(textField_awakeFromNib), method_getImplementation(method6), method_getTypeEncoding(method6));
     }
     
-    
-    Method method7 = class_getInstanceMethod([UITextView class], @selector(button_awakeFromNib));
+    Method method7 = class_getInstanceMethod([UITextView class], @selector(textView_awakeFromNib));
     Method method8 = class_getInstanceMethod([UITextView class], @selector(awakeFromNib));
-    
     if (!class_addMethod([UITextView class], @selector(awakeFromNib), method_getImplementation(method7), method_getTypeEncoding(method7))) {
         method_exchangeImplementations(method8, method7);
     } else {
         class_replaceMethod(self, @selector(textView_awakeFromNib), method_getImplementation(method8), method_getTypeEncoding(method8));
     }
+    
+    Method method9 = class_getInstanceMethod([UISearchBar class], @selector(searchBar_awakeFromNib));
+    Method method10 = class_getInstanceMethod([UISearchBar class], @selector(awakeFromNib));
+    if (!class_addMethod([UISearchBar class], @selector(awakeFromNib), method_getImplementation(method9), method_getTypeEncoding(method9))) {
+        method_exchangeImplementations(method10, method9);
+    } else {
+        class_replaceMethod(self, @selector(searchBar_awakeFromNib), method_getImplementation(method10), method_getTypeEncoding(method10));
+    }
+
+    
     
     
 }
@@ -95,4 +103,19 @@
     UITextView * textView = (UITextView *)self;
     textView.font = [UIFont fontWithDescriptor:textView.font.fontDescriptor size:textView.font.pointSize * SizeScale];
 }
+
+/*
+ UISearchBar
+ 方法交换以后 当系统调用 awakeFromNib 方法的时候实际上会调用 searchBar_awakeFromNib 方法
+ **/
+- (void)searchBar_awakeFromNib {
+    [self searchBar_awakeFromNib];
+    UISearchBar * searchBar = (UISearchBar *)self;
+    UITextField *textField = [searchBar valueForKey:@"_searchField"];
+    textField.font = [UIFont fontWithDescriptor:textField.font.fontDescriptor size:textField.font.pointSize * SizeScale];
+}
+
+
+
+
 @end
